c7301ec982cdcde154c5057a4a59e0fbe528b1e7
Roach <climbingroach@gmail.com>
2017-11-29 12:48:14 -0700

    added view.jinja2 and edit.jinja2 for our urls modified defualt.py to include the view configs for view wiki,page,...added routes for those files we got rid of mymodels and the metatemplate and added user, and page to models. note the system did not run until we ran initialize_Banner_db development.ini from our virtualenv/bin/ folder

--- banner/routes.py
+++ banner/routes.py
@@ -1,7 +1,14 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('course', '/courses')
-    config.add_route('professor', '/professors')
-    config.add_route('section', '/sections')
+    config.add_route('view_wiki', '/')
+    # this was added from https://docs.pylonsproject.org/projects/pyramid/en/latest/tutorials/wiki2/definingviews.html
+    # to understand how routes are mapped to views
+    # note the order must be this way to enable editing we must first allow add page
+    config.add_route('view_page', '/{pagename}')
+    config.add_route('add_page', '/add_page/{pagename}')
+    config.add_route('edit_page', '/{pagename}/edit_page')
+    # these are the routes I think I need for the banner system based on my banner_models.py
+    #config.add_route('course', '/courses')
+    #config.add_route('professor', '/professors')
+    #config.add_route('section', '/sections')
 
